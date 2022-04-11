db3cbe3934de709575717584251eba11aab90b13
moraes <moraeslua66@gmail.com>
2022-04-11 09:13:28 -0300

    feat: imagem cafezinho no Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import Spiner from '../images/Spinner.svg';
+import Waiting from '../images/waiting.svg';
 
 const LoadingDiv = styled.div`
   min-height: 515px;
@@ -13,6 +14,7 @@ const LoadingDiv = styled.div`
 export default function Loading() {
   return (
     <LoadingDiv>
+      <img src={ Waiting } alt="Spiner" width="150px" height="150px" />
       <img src={ Spiner } alt="Spiner" />
     </LoadingDiv>
   );
