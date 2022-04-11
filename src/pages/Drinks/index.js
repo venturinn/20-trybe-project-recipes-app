a70b9e91478c9ec4f2274e5e89764c51e0bc73ad
kodenart <mjuanvitorp@gmail.com>
2022-04-11 11:11:46 -0300

    refactor: mudança na implementação do Loading

--- src/pages/Drinks/index.js
+++ src/pages/Drinks/index.js
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import BottomNav from '../../components/BottomNav';
 import Header from '../../components/Header';
 import RecipesList from '../../components/RecipesList';
 import Filters from '../../components/Filters';
 import { setMainPageRecipes, cleanUpMainPage } from '../../redux/actions/mainPage';
+import Loading from '../../components/Loading';
 
 export default function Drinks() {
   const dispatch = useDispatch();
   const location = useLocation();
+  const isLoading = useSelector((state) => state.loading);
   useEffect(() => {
     const cameFrom = location.state ? location.state.from : undefined;
     if (cameFrom !== 'ingredient') {
@@ -22,7 +24,7 @@ export default function Drinks() {
     <section>
       <Header />
       <Filters />
-      <RecipesList />
+      {isLoading ? <Loading /> : <RecipesList />}
       <BottomNav />
     </section>
   );
